6e75490a0a133ef32eb1f68c8ca62271bc1975da
PurpleGodzilla <romgrom90@yandex.ru>
2023-03-31 01:28:49 +0300

    Homework 3.6
    Homework 3.6

---
+++ project3.6(2).cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main()
+{
+	int a=6;
+	while(a!=7)
+	{
+		scanf("%i",&a);
+		if(a>7)
+		{
+			printf("bolshe\n");
+		}
+		else
+		{
+			if(a<7)
+			{
+					printf("menshe\n");
+			}
+		
+		}
+	}
+	printf("SUPER");
+	
+}
+
